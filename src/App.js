5e315692d2ba0e261c52894c32d690f5aa901b2d
byio <ben@benyang.io>
2018-02-23 00:00:24 +0800

    render header component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import { Provider } from 'react-redux';
 
 import reducers from './reducers';
 
+import { Header } from './components/common';
+
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText="techStack"/>
+      </View>
     </Provider>
   );
 };
